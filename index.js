7da8175c9921b55fb2e335f3bc0b92133b64a004
TheWisePigeon <zozozozeph@gmail.com>
2022-02-25 10:31:26 +0000

    did stuff

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-const puppeteer = require('puppeteer');
-const search= "site:stackoverflow.com undefined index php";
+const axios = require('axios').default
 
-(async () => {
-    const browser = await puppeteer.launch()
-    const page= await browser.newPage()
-    await page.goto(`https://www.google.com/search?q=${search}`, {
-        waitUntil: 'networkidle2'
+
+axios
+    .get("https://google.com/search?q=tom+hardy")
+    .then(async (res) => {
+      data = await res.data
+      console.log(data);
     })
-    await page.pdf({ path: 'bruh.pdf', format: 'a4'})
-    await browser.close()
-})()
-
-try {
-    
-} catch (error) {
-    async () => {
-      const browser = await puppeteer.launch()
-      const page = await browser.newPage()
-      await page.goto(`https://www.google.com/search?q=${error}`,{
-          waitUntil: 'networkidle0'
-      })
-      await page.pdf({ path: 'err.pdf', format: 'a4'})
-      await browser.close()
-    }
-}
